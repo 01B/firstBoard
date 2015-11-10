da04e879e0c0cc71a54ec2581407b5d80e59ebea
01B <no.pain.no.code@gmail.com>
2015-11-11 06:15:51 +0900

    refactoring

--- src/main/java/net/nopainnocode/firstboard/FirstBoardApplication.java
+++ src/main/java/net/nopainnocode/firstboard/FirstBoardApplication.java
@@ -1,17 +1,9 @@
 package net.nopainnocode.firstboard;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
-import org.springframework.web.context.request.WebRequestInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 public class FirstBoardApplication {
@@ -24,42 +16,4 @@ public class FirstBoardApplication {
     ObjectMapper objectMapper(){
         return new ObjectMapper();
     }
-
-
-    @Bean
-    WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-
-            @Override
-            public void addInterceptors(InterceptorRegistry registry) {
-                registry.addWebRequestInterceptor(oeiv());
-            }
-        };
-    }
-
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-
-    @Bean
-    public WebRequestInterceptor oeiv() {
-        OpenEntityManagerInViewInterceptor oeiv = new OpenEntityManagerInViewInterceptor();
-        oeiv.setEntityManagerFactory(entityManagerFactory);
-        return oeiv;
-    }
-
-    // todo : to check the validation add validation annotation to domain classes
-
-    // todo : exception control
-
-    // todo : reflection to copy a class object
-
-    // todo : security
-
-    // todo : authority
-
-    // todo : study
-
-    // what is ResponseEntity, ReponseEntity.ok, ReponseEntity.status(HttpStatus).Header or Body
-    // ListResponse
-
 }
