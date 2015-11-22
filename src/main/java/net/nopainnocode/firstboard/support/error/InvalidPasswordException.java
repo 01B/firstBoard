6cc097042f879a1aff321af982f0d4c5c8be515d
01B <no.pain.no.code@gmail.com>
2015-11-23 05:49:29 +0900

    add InvalidPasswordException

---
+++ src/main/java/net/nopainnocode/firstboard/support/error/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package net.nopainnocode.firstboard.support.error;
+
+/**
+ * Created by no_pain_no_code on 2015. 11. 23..
+ */
+public class InvalidPasswordException extends RuntimeException{
+
+    public InvalidPasswordException(){}
+}
